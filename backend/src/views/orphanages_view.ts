99a010e78f1e2c47ad0ef533814a9fcd0ce611a2
Zackwn <rafaelfavazzi@gmail.com>
2020-10-14 16:23:43 -0300

    add longitude on orphanage view and fix open_on_weekends

--- backend/src/views/orphanages_view.ts
+++ backend/src/views/orphanages_view.ts
@@ -7,6 +7,7 @@ export default {
             id: orphanage.id,
             name: orphanage.name,
             latitude: orphanage.latitude,
+            longitude: orphanage.longitude,
             about: orphanage.about,
             instructions: orphanage.instructions,
             opening_hours: orphanage.opening_hours,
