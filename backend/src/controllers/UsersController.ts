38ef3e930c0a9d9fe74eba09b1fa8e67b968fb72
Zackwn <rafaelfavazzi@gmail.com>
2020-11-23 13:29:30 -0300

    create user

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express'
+import { hash } from 'argon2'
+import { getRepository } from 'typeorm'
+import User from '../models/Users'
 
 export class UsersController {
     async create(req: Request, res: Response) {
@@ -8,8 +11,19 @@ export class UsersController {
             name
         } = req.body
 
-        // create user
+        const hashPassword = await hash(password)
 
-        return res.status(201).send()
+        const userData = { email, password: hashPassword, name }
+
+        const userRepository = getRepository(User)
+
+        const newUser = userRepository.create(userData)
+
+        await userRepository.save(newUser)
+
+        return res.status(201).json({
+            message: 'Successfuly created!',
+            user: newUser
+        })
     }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,16 +4,20 @@ import multerConfig from './config/upload'
 
 // controllers
 import { OrphanagesController } from './controllers/OrphanagesController'
+import { UsersController } from './controllers/UsersController'
 
 const orphanagesController = new OrphanagesController()
+const usersController = new UsersController()
 
 const routes = Router()
 const upload = multer(multerConfig)
 
+/* Orphanages */
 routes.get('/orphanages', orphanagesController.index)
-
 routes.get('/orphanages/:id', orphanagesController.show)
-
 routes.post('/orphanage', upload.array('images'), orphanagesController.create)
+
+/* Users */
+routes.post('/user', usersController.create)
 
 export default routes
