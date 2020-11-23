5918c13066d8b0dd505dc87ea34fb7cdcd360fed
Zackwn <rafaelfavazzi@gmail.com>
2020-11-23 14:33:29 -0300

    login

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express'
-import { hash } from 'argon2'
+import {
+    hash as generatePasswordHash,
+    verify as verifyPassword
+} from 'argon2'
 import { getRepository } from 'typeorm'
 import User from '../models/Users'
+import { JWTHelperInterface } from '../helpers/jwt'
 
 export class UsersController {
+    jwtHelper: JWTHelperInterface
+
+    constructor(jwtHelper: JWTHelperInterface) {
+        this.jwtHelper = jwtHelper
+    }
+
     async create(req: Request, res: Response) {
         const {
             email,
@@ -11,7 +21,7 @@ export class UsersController {
             name
         } = req.body
 
-        const hashPassword = await hash(password)
+        const hashPassword = await generatePasswordHash(password)
 
         const userData = { email, password: hashPassword, name }
 
@@ -25,5 +35,34 @@ export class UsersController {
             message: 'Successfuly created!',
             user: newUser
         })
+    }
+
+    async login(req: Request, res: Response) {
+        const { email, password } = req.body
+
+        const userRepository = getRepository(User)
+
+        // verify if user exists
+        const user = await userRepository.findOne({ where: { email } })
+
+        if (!user) {
+            return res.status(400).json({
+                errors: {
+                    email: 'No registered user with this email'
+                }
+            })
+        }
+
+        // verify if password is correct
+        const isPasswordCorrect = await verifyPassword(user.password, password)
+
+        if (!isPasswordCorrect) {
+            return res.status(400).send()
+        }
+
+        // login user
+        const token = this.jwtHelper.sign({ userId: user.id })
+
+        return res.json(token)
     }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,12 +2,17 @@ import { Router } from 'express'
 import multer from 'multer'
 import multerConfig from './config/upload'
 
+// helpers
+import { JWTHelper } from './helpers/jwt'
+
 // controllers
 import { OrphanagesController } from './controllers/OrphanagesController'
 import { UsersController } from './controllers/UsersController'
 
+const jwtHelper = new JWTHelper()
+
 const orphanagesController = new OrphanagesController()
-const usersController = new UsersController()
+const usersController = new UsersController(jwtHelper)
 
 const routes = Router()
 const upload = multer(multerConfig)
@@ -18,6 +23,7 @@ routes.get('/orphanages/:id', orphanagesController.show)
 routes.post('/orphanage', upload.array('images'), orphanagesController.create)
 
 /* Users */
-routes.post('/user', usersController.create)
+routes.post('/user', (req, res) => usersController.create(req, res))
+routes.post('/user/login', (req, res) => usersController.login(req, res))
 
 export default routes
