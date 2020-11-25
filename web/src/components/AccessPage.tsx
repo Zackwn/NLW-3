41fe1e40694d4a2e163f37df02de75457bbe0a27
Zackwn <rafaelfavazzi@gmail.com>
2020-11-25 16:37:31 -0300

    go back button

--- web/src/components/AccessPage.tsx
+++ web/src/components/AccessPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 import '../styles/components/access-page.css'
 
 import BigLogo from '../assets/full-logo.svg'
+import { FiArrowLeft } from 'react-icons/fi'
 
 const AccessPage: React.FC = ({ children }) => {
+   const { goBack } = useHistory()
+
    return (
       <div id='access-page-container'>
          <div id='presentation'>
@@ -16,6 +20,9 @@ const AccessPage: React.FC = ({ children }) => {
             </div>
          </div>
          <div id='form'>
+            <button id='go-back-button' type='button' onClick={goBack}>
+               <FiArrowLeft color='#15C3D6' />
+            </button>
             {children}
          </div>
       </div>
