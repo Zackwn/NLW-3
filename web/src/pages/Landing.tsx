9a07b95cdac988b4e73432d2ae38965c8b3bb8d5
Zackwn <rafaelfavazzi@gmail.com>
2020-11-25 16:18:37 -0300

    Landing page ui update

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -6,27 +6,38 @@ import logoImg from '../assets/logo.svg'
 import { FiArrowRight } from 'react-icons/fi'
 
 const Landing: React.FC = () => {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
+   return (
+      <div id="page-landing">
+         <div className="content-wrapper">
+            <div id="landing-header">
+               <div id='logo-location-wrapper'>
+                  <img src={logoImg} alt="Happy" />
 
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
+                  <div className="location">
+                     <strong>Rio do Sul</strong>
+                     <span>Santa Catarina</span>
+                  </div>
+               </div>
 
-        <div className="location">
-          <strong>Sua cidade</strong>
-          <span>seu estado</span>
-        </div>
+               <Link to='/login'>
+                  <button id='access-restrict-button'>
+                     Acesso Restrito
+               </button>
+               </Link>
 
-        <Link to='/orphanages' className='enter-app'>
-          <FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
-        </Link>
+            </div>
+
+            <main>
+               <h1>Leve felicidade para o mundo</h1>
+               <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+            </main>
+
+            <Link to='/orphanages' className='enter-app'>
+               <FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
+            </Link>
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 export default Landing;
