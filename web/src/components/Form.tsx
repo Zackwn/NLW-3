20cb264dc3b51465002264f79c78e0f0c092d55b
Zackwn <rafaelfavazzi@gmail.com>
2020-11-26 17:09:28 -0300

    titles limit width

--- web/src/components/Form.tsx
+++ web/src/components/Form.tsx
@@ -10,18 +10,20 @@ interface FormProps extends React.DetailedHTMLProps<React.FormHTMLAttributes<HTM
 const Form: React.FC<FormProps> = ({ children, formTitle, formSubTitle, ...formProps }) => {
    return (
       <form id='form-component' {...formProps}>
-         <h1
-            className={formSubTitle ? '' : 'margin-bottom'}
-            id='form-title'
-         >{formTitle}</h1>
-         {formSubTitle
-            ? (
-               <h2
-                  id='form-subtitle'
-                  className='margin-bottom'
-               >{formSubTitle}</h2>
-            ) : null
-         }
+         <div id='form-component-titles-wrapper'>
+            <h1
+               className={formSubTitle ? '' : 'margin-bottom'}
+               id='form-title'
+            >{formTitle}</h1>
+            {formSubTitle
+               ? (
+                  <h2
+                     id='form-subtitle'
+                     className='margin-bottom'
+                  >{formSubTitle}</h2>
+               ) : null
+            }
+         </div>
          {children}
       </form>
    )
