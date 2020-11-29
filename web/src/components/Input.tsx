ebe86e6e26a292f9555a20a4940e568ac700f24b
Zackwn <rafaelfavazzi@gmail.com>
2020-11-29 14:09:29 -0300

    input error

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -3,14 +3,21 @@ import React from 'react';
 import '../styles/components/input.css'
 
 interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-   labelText: string
+   labelText: string,
+   error?: string
 }
 
-const Input: React.FC<InputProps> = ({ children, labelText, ...inputProps }) => {
+const Input: React.FC<InputProps> = ({ children, labelText, error, ...inputProps }) => {
    return (
       <div className='input-component'>
-         <label>{labelText}</label>
-         <input {...inputProps} />
+         <label
+            className={error ? 'with-error' : ''}
+         >{labelText}</label>
+         <input
+            className={error ? 'with-error' : ''}
+            {...inputProps}
+         />
+         {error ? <span>{error}</span> : null}
       </div>
    )
 }
