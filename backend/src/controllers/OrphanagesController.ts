1e2f622e48d79766ad8ec9471e2b0406635fe708
Zackwn <rafaelfavazzi@gmail.com>
2020-12-07 14:44:16 -0300

    fix formData boolean as string error

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -48,7 +48,7 @@ export class OrphanagesController {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends: open_on_weekends === "true"
         }
 
         let deleteImagesResult: Promise<DeleteResult>[]
