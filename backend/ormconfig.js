083aa829bc77949c862dcf4de7748a2192fdd180
Zackwn <rafaelfavazzi@gmail.com>
2020-12-20 14:25:00 -0300

    ormconfig in js file

---
+++ backend/ormconfig.js
@@ -0,0 +1,14 @@
+export default {
+    type: "sqlite",
+    database: "./src/database/database.sqlite",
+    migrations: [
+        "./src/database/migrations/*.ts"
+    ],
+    entities: [
+        "./src/models/*.ts"
+    ],
+    cli: {
+        migrationsDir: "./src/database/migrations"
+    },
+    logging: true
+}
