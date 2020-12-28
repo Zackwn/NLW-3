1297e528e14d02f2a33f332f9a82aba89a3daf44
Zackwn <rafaelfavazzi@gmail.com>
2020-12-28 14:19:19 -0300

    rename file

---
+++ backend/src/utils/orphanageIsFromUserOrFail.ts
@@ -0,0 +1,33 @@
+import { AppError } from '../errors/appError'
+import Orphanage from '../models/Orphanages'
+import { UserRole } from '../models/Users'
+
+interface isOrphanageFromUserOptions {
+    ignoreIfAdmin: boolean
+}
+
+interface isOrphanageFromUserUserParams {
+    userRole: UserRole,
+    userId: number
+}
+
+/**
+ * @param options ignoreIfAdmin default value is false
+ * @returns If orphanage is from user will return void. if orphanage is not from user an response with 403 status will be send
+ */
+export async function orphanageIsFromUserOrFail(
+    { userId, userRole }: isOrphanageFromUserUserParams,
+    creatorId: number,
+    options?: isOrphanageFromUserOptions
+): Promise<void> {
+    if (userRole === UserRole.ADMIN && options.ignoreIfAdmin) {
+        return
+    }
+
+    // check if user own the orphanage
+    if (creatorId !== userId) {
+        throw new AppError(403)
+    }
+
+    return
+}
