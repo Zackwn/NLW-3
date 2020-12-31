28aa23796f96096ce095b8e17d56eb0a16cdf8a6
Zackwn <rafaelfavazzi@gmail.com>
2020-12-31 15:25:53 -0300

    use dashboard content component

--- web/src/pages/UserDashboard/Pending.tsx
+++ web/src/pages/UserDashboard/Pending.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { FiArrowRight } from 'react-icons/fi'
 import { useHistory } from 'react-router-dom'
 import { OrphanageInterface, OrphanageParams } from '../../@types/orphanage'
+import DashboardContentLayout from '../../components/Dashboard/DashboardContentLayout'
 import OrphanageModal from '../../components/Orphanage/OrphanageModal'
 import api from '../../services/api'
-
-import '../../styles/pages/dashboard/index.css'
 
 const Pending: React.FC = () => {
    const { push: historyPush } = useHistory()
@@ -22,34 +21,27 @@ const Pending: React.FC = () => {
    }, [])
 
    return (
-      <div id='dashboard-content-container'>
-         <div id='dashboard-content-header'>
-            <h1 className='dashboard-content-title'>
-               Cadastros pendentes
-            </h1>
-            <span>{orphanages?.length} orfanatos</span>
-         </div>
-
-         <div id='orphanages-modal-wrapper'>
-            {orphanages?.map(orphanage => {
-               return (
-                  <OrphanageModal
-                     orphanage={orphanage}
-                  >
-                     <div
-                        className='icon-button'
-                        onClick={() =>
-                           historyPush('/orphanages/detail/', {
-                              orphanage: orphanage
-                           } as OrphanageParams)
-                        }>
-                        <FiArrowRight color='#15C3D6' size={26} />
-                     </div>
-                  </OrphanageModal>
-               )
-            })}
-         </div>
-      </div>
+      <DashboardContentLayout
+         header={{ title: 'Cadastros Pendentes', length: orphanages?.length }}
+      >
+         {orphanages?.map(orphanage => {
+            return (
+               <OrphanageModal
+                  orphanage={orphanage}
+               >
+                  <div
+                     className='icon-button'
+                     onClick={() =>
+                        historyPush('/orphanages/detail/', {
+                           orphanage: orphanage
+                        } as OrphanageParams)
+                     }>
+                     <FiArrowRight color='#15C3D6' size={26} />
+                  </div>
+               </OrphanageModal>
+            )
+         })}
+      </DashboardContentLayout >
    )
 }
 
--- web/src/pages/UserDashboard/Registered.tsx
+++ web/src/pages/UserDashboard/Registered.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
-import '../../styles/pages/dashboard/index.css'
 import OrphanageModal from '../../components/Orphanage/OrphanageModal'
 import { FiEdit3, FiTrash } from 'react-icons/fi'
 import api from '../../services/api'
 import { OrphanageInterface } from '../../@types/orphanage'
+import DashboardContentLayout from '../../components/Dashboard/DashboardContentLayout'
 
 const Registered: React.FC = () => {
    const history = useHistory()
@@ -26,40 +26,31 @@ const Registered: React.FC = () => {
    }
 
    return (
-      <div id='dashboard-content-container'>
-         <div id='dashboard-content-header'>
-            <h1 className='dashboard-content-title'>
-               Orfanatos Cadastrados
-            </h1>
-            <span>{orphanages?.length} orfanatos</span>
-         </div>
-
-         <div id='orphanages-modal-wrapper'>
-            {orphanages && orphanages.map((orphanage) => {
-               return (
-                  <OrphanageModal
-                     key={orphanage.id}
-                     orphanage={orphanage}
+      <DashboardContentLayout
+         header={{ title: 'Orfanatos Cadastrados', length: orphanages?.length }}
+      >
+         {orphanages && orphanages.map((orphanage) => {
+            return (
+               <OrphanageModal
+                  key={orphanage.id}
+                  orphanage={orphanage}
+               >
+                  <div
+                     className='icon-button'
+                     onClick={() => handleUpdateOrphanage(orphanage)}
                   >
-                     <div
-                        className='icon-button'
-                        onClick={() => handleUpdateOrphanage(orphanage)}
-                     >
-                        <FiEdit3 color='#15C3D6' size={26} />
-                     </div>
-                     <div
-                        className='icon-button'
-                        onClick={() => handleUpdateOrphanage(orphanage)}
-                     >
-                        <FiTrash color='#15C3D6' size={26} />
-                     </div>
-                  </OrphanageModal>
-               )
-            })}
-
-         </div>
-
-      </div>
+                     <FiEdit3 color='#15C3D6' size={26} />
+                  </div>
+                  <div
+                     className='icon-button'
+                     onClick={() => handleUpdateOrphanage(orphanage)}
+                  >
+                     <FiTrash color='#15C3D6' size={26} />
+                  </div>
+               </OrphanageModal>
+            )
+         })}
+      </DashboardContentLayout>
    )
 }
 
